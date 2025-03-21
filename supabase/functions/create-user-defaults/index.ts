d514a5423debaf590c1bf06f3e63382dedcbb9d8
ahsandevhub <contact2ahsanhabib@gmail.com>
2025-03-21 19:13:30 +0600

    env file removed

--- supabase/functions/create-user-defaults/index.ts
+++ supabase/functions/create-user-defaults/index.ts
@@ -1,9 +1,15 @@
-Deno.serve(async (req) => {
-    const { email } = await req.json()
-    const data = {
-      message: `Hello ${email}!`,
-    }
-  
-    return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } })
-  })
-  
+import { createClient } from 'jsr:@supabase/supabase-js@2';
+
+Deno.serve(async (req: Request) => {
+
+  const supabaseClient = createClient(
+    Deno.env.get('SUPABASE_URL') ?? '',
+    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+  );
+
+  // Get the session or user object
+  const authHeader = req.headers.get('Authorization')!;
+  const token = authHeader.replace('Bearer ', '');
+  const { data } = await supabaseClient.auth.getUser(token);
+
+})
